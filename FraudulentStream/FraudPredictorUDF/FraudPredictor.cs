560e8a434ab218a6d5d12face2bac13303e93168
Alexander Slotte <alexander.slotte@gmail.com>
2020-03-18 07:55:45 -0400

    Added UDF project

---
+++ FraudulentStream/FraudPredictorUDF/FraudPredictor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.ML;
+using System.Linq;
+using System.Reflection;
+
+namespace FraudPredictor
+{
+    public class FraudPredictor
+    {
+        public static bool Predict(string type,
+            string destinationAccountName,
+            double amount,
+            double oldBalanceOfOriginAccount,
+            double oldBalanceOfDestinationAccount,
+            double newBalanceOfOriginAccount,
+            double newBalanceOfDestinationAccount)
+        {
+            MLContext mlContext = new MLContext();
+
+            var model = LoadModel(mlContext);
+
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(model);
+
+            var input = new ModelInput
+            {
+                Type = type,
+                NameDest = destinationAccountName,
+                Amount = (float)amount,
+                OldbalanceDest = (float)oldBalanceOfDestinationAccount,
+                OldbalanceOrg = (float)oldBalanceOfOriginAccount,
+                NewbalanceDest = (float)newBalanceOfDestinationAccount,
+                NewbalanceOrig = (float)newBalanceOfOriginAccount
+            };
+
+            ModelOutput result = predEngine.Predict(input);
+            return result.Prediction;
+        }
+
+        private static ITransformer LoadModel(MLContext mlContext)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var resource = assembly.GetManifestResourceNames().First(x => x.EndsWith("MLModel.zip"));
+
+            using (var stream = assembly.GetManifestResourceStream(resource))
+            {
+                return mlContext.Model.Load(stream, out var _);
+            }
+        }
+    }
+}
---
+++ FraudulentStream/FraudPredictorUDF/ModelOutput.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.ML.Data;
+
+namespace FraudPredictor
+{
+    public class ModelOutput
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Score { get; set; }
+    }
+}
